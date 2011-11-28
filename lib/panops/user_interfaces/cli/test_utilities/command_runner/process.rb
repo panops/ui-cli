c0a512a8d17864e56134b6794036d093b658325b
Justin Lynn <j@3crowd.com>
2011-11-28 13:31:06 -0800

    process needs to know what states the underlying system process might be in

--- lib/panops/user_interfaces/cli/test_utilities/command_runner/process.rb
+++ lib/panops/user_interfaces/cli/test_utilities/command_runner/process.rb
@@ -9,6 +9,19 @@ module PanOps
           # Represents a spawned command. Has IO Endpoints for communicating
           # with the spawned process.
           class Process
+            
+            def initialize
+              initialize_state_machines
+            end
+            
+            state_machine :state, :initial => :created do
+              
+              state :running
+              
+              state :terminated
+              
+            end
+            
           end
 
         end
--- spec/unit/panops/user_interfaces/cli/test_utilities/command_runner/process_spec.rb
+++ spec/unit/panops/user_interfaces/cli/test_utilities/command_runner/process_spec.rb
@@ -3,7 +3,52 @@ require 'unit/unit_helper'
 describe PanOps::UserInterfaces::CLI::TestUtilities::CommandRunner::Process do
   
   it 'is defined' do
-    subject.should_not be_nil
+    subject.class.should be_eql(PanOps::UserInterfaces::CLI::TestUtilities::CommandRunner::Process)
+  end
+  
+  context 'an instance' do
+    
+    context 'a state machine called state' do
+      
+      before :each do
+        @state_state_machine = subject.class.state_machines[:state]
+        @state_state_machine_states = @state_state_machine.states.map{|s| s.name}
+      end
+      
+      it 'is defined' do
+        @state_state_machine.should_not be_nil
+      end
+      
+      context "for state 'created'" do
+        
+        it "defines that state" do
+          @state_state_machine_states.should include(:created)
+        end
+        
+        it "is initially in state 'created'" do
+          subject.state_name.should eql(:created)
+        end
+        
+      end
+      
+      context "for state 'running'" do
+      
+        it "defines that state" do
+          @state_state_machine_states.should include(:running)
+        end
+        
+      end
+      
+      context "for state 'terminated'" do
+      
+        it "defines a state 'terminated'" do
+          @state_state_machine_states.should include(:terminated)
+        end
+        
+      end
+      
+    end
+    
   end
   
 end
